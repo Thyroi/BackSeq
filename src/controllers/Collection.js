067014154778ba5736998b99fd204bfd5b352e32
revivocelulares <71577083+revivocelulares@users.noreply.github.com>
2022-04-02 19:48:09 -0300

    push

---
+++ src/controllers/Collection.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const { Sequelize, Op } = require('sequelize');
+const { Products, Collection } = require('../db.js');
+
+const season = [
+  { id_collection: 1, name: 'Spring'},
+  { id_collection: 2, name: 'Summer'},
+  { id_collection: 3, name: 'Autunm'},
+  { id_collection: 4, name: 'Winter'}
+];
+
+const collection = {
+  addCollection: async (req, res) => {
+    try {
+      let temporada = Collection.bulkCreate(season);
+      res.status(200).json(temporada);
+    }
+    catch (error) {
+      console.log(error);
+      res.redirect(404, '../');
+    }
+  },
+
+}
+
+module.exports = collection;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const products = require('./products');
 const populate = require('./populate.js');
+const collection = require('./collection');
 
 router.use('/populate', populate);
 router.use('/products', products);
+router.use('/collection', collection);
 
 
 module.exports = router;
