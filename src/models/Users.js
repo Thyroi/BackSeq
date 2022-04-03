c03507bfc31238d44ea3118cae23112edd5ae272
revivocelulares <71577083+revivocelulares@users.noreply.github.com>
2022-04-02 23:05:49 -0300

    Update Users.js

--- src/models/Users.js
+++ src/models/Users.js
@@ -3,7 +3,7 @@ module.exports = (sequelize) => {
   sequelize.define('Users', {
       id_user: {
           type: DataTypes.INTEGER(),
-          autoincrement: true,
+          autoIncrement: true,
           allowNull: false,
           primaryKey: true,
           unique: true
