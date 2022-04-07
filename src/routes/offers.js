f6a28a4d508284c9b4f348ee2866070852daa0ee
Juan José Salgado Niño <thyroi0208@gmail.com>
2022-04-07 14:17:18 -0500

    Nodemailer

--- src/routes/offers.js
+++ src/routes/offers.js
@@ -1,10 +1,14 @@
 const route = require("express").Router();
-const { Hora, genOffer} = require('../controllers/Offers');
+const { mailer } = require("../controllers/Mailer");
+const { Hora, genOffer } = require('../controllers/Offers');
 
- route.get("/hora", async (req, res)=>{
-     const day = await Hora();
-     const info = await genOffer(day);
+route.get("/hora", async (req, res) => {
+    const day = await Hora();
+    const info = await genOffer(day);
     return res.json(info)
- });
-
+});
+route.get("/email", async (req, res) => {
+    const mail = await mailer();
+    return res.json(mail)
+});
 module.exports = route;
