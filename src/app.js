ecca0fdadba44dd08c9d2dbf217a7a2012c66f70
revivocelulares <71577083+revivocelulares@users.noreply.github.com>
2022-04-09 17:41:26 -0300

    update MeLi

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+require('dotenv').config();
+const mercadopago = require("mercadopago");
+const cors = require("cors");
 
 require('./db.js');
 
@@ -9,6 +12,7 @@ const server = express();
 
 server.name = 'FRAILEJON ERNESTO PEREZ';
 
+server.use(cors());
 server.use(express.json());
 server.use(cookieParser());
 server.use(morgan('dev'));
@@ -27,6 +31,38 @@ server.use((err, req, res, next) => {
   const message = err.message || err;
   console.error(err);
   res.status(status).send(message);
+});
+
+server.post("/process-payment", (req, res) => {
+    mercadopago.configurations.setAccessToken(process.env.ACCESS_TOKEN_MP);
+    const payment_data = {
+        transaction_amount: req.body.transaction_amount,
+        token: req.body.token,
+        description: req.body.description,
+        installments: Number(req.body.installments),
+        payment_method_id: req.body.paymentMethodId,
+        issuer_id: req.body.issuer,
+        payer: {
+            email: req.body.payer.email,
+            identification: {
+                type: req.body.payer.docType,
+                number: req.body.payer.docNumber,
+            },
+        },
+    };
+
+    mercadopago.payment
+        .save(payment_data)
+        .then((response) => {
+            return res.status(response.status).json({
+                id: response.body.id,
+                status: response.body.status,
+                status_detail: response.body.status_detail,                
+            });
+        })
+        .catch((err) => {
+            return res.status(500).send(err);
+        });
 });
 
 module.exports = server;
