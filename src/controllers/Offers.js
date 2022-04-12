f3d013923ff743a5e63f8129883f4f5559af9d25
Juan José Salgado Niño <thyroi0208@gmail.com>
2022-04-12 01:32:20 -0500

    Offers routes

--- src/controllers/Offers.js
+++ src/controllers/Offers.js
@@ -2,17 +2,13 @@ const { Sequelize, Op } = require('sequelize');
 const { Products, Category, Collection } = require('../db.js');
 
 module.exports = {
-    Hora : async() =>{
-        let date = new Date()
-        return date.getDay()
-    },
-    genOffer: async(id) => {
+    getItems: async() => {
         let date = new Date()
         let hola = parseInt(date.getDay())
 
-        const women = await Category.findAll({
+        const products = await Category.findAll({
             where: {
-                CategoryIdCategory: hola
+                id_category: 3
             },
             include: [{
                 model: Products,
@@ -24,10 +20,29 @@ module.exports = {
                 }
             }]
         });
-        const offer = women.map(e => {
-            return "hola"
-        })
-
-        return offer
+        let ids = []
+        let prices = []
+        products.forEach(e => e.Products.forEach(a => {
+            let price = a.price *((100-10)/100);
+            ids.push(a.id_product);
+            prices.push(price)
+        }))
+        return {ids, prices}
+    },
+    genOffer: async(info) => {
+        const items = [];
+        for (let i = 0; i < info.ids.length; i++) {
+            let answer = await Products.update(
+                { is_offer: true, price_offer: info.prices[i]},
+                {
+                    where:{
+                        id_product: info.ids[i]
+                    }
+                } 
+            );
+            items.push(answer)
+        }
+        
+        return items
     }
 };
