728222092b33526722c8fb8603802432dc487eb6
revivocelulares <71577083+revivocelulares@users.noreply.github.com>
2022-04-18 15:04:20 -0300

    update

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const route = require("express").Router();
-const { Client } = require('../db.js');
+const { Client, Users } = require('../db.js');
 require('dotenv').config();
 
 
@@ -11,9 +11,29 @@ route.post('/', async (req, res) => {
       login_name: login_name
     }
   });
-  jwt.sign({client}, process.env.SECRET_KEY, (err, token) => {
-    res.json({token, client});
-  })
+  if(client){
+    jwt.sign({client}, process.env.SECRET_KEY, (err, token) => {
+      res.json({token, client});
+    })
+  } else {
+    res.json({message: "Incorrect login name or password"});
+}
+});
+
+route.post('/admin', async (req, res) => {
+  const {user_name} = req.body;
+  const user = await Users.findOne({
+    where: {
+      user_name: user_name
+    }
+  });
+  if(user) {
+    jwt.sign({user}, process.env.SECRET_KEY, (err, token) => {
+      res.json({token, user});
+    })
+  } else {
+    res.json({message: "Incorrect user or password"});
+  }
 });
 
 module.exports = route;
