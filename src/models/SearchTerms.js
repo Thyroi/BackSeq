abec232887be0c682a1173d13a436d9007688a38
llamagustin <86086266+llamagustin@users.noreply.github.com>
2022-04-18 21:28:15 -0300

    cosos

--- src/models/SearchTerms.js
+++ src/models/SearchTerms.js
@@ -1,24 +1,15 @@
 const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
-    sequelize.define('Review', {
-        review: {
-            type: DataTypes.INTEGER,
+    sequelize.define('SearchTerms', {
+        term: {
+            type: DataTypes.STRING,
             allowNull: false,
             primaryKey: true,
-            autoIncrement: true,
             unique: true
         },
-        stars: {
+        count: {
             type: DataTypes.INTEGER,
-            allowNull: false,
-            validate: {
-                min: 1,
-                max: 5
-            }
+            allowNull: false
         },
-        description: {
-            type: DataTypes.TEXT(),
-            allowNull: false,
-        }
     }, { timestamps: true });
 };
