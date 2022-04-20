f1f26c655745e9370f4fe2cdda1e71227001f7fb
revivocelulares <71577083+revivocelulares@users.noreply.github.com>
2022-04-20 16:16:50 -0300

    Update offers.js

--- src/routes/offers.js
+++ src/routes/offers.js
@@ -1,21 +1,44 @@
 const route = require("express").Router();
 const mailer = require("../controllers/Mailer");
 const { getItems, genOffer, newOffer } = require('../controllers/Offers');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const verify_admin_token = require('../controllers/verify_admin_token.js');
 
-route.patch("/setoff", async (req, res) => {
-    const info = await getItems()
-    const updated = await genOffer(info);
-    return res.json(updated)
+route.patch("/setoff", verify_admin_token, async (req, res) => {
+  jwt.verify(req.token, process.env.SECRET_KEY, async(error, authData) => {
+    if(error){
+      console.log(error);
+      res.status(403).send({message:"Forbidden Access"});
+    } else {
+      const info = await getItems()
+      const updated = await genOffer(info);
+      return res.json(updated)
+    }
+  })
 });
-route.get("/email", async (req, res) => {
-    let info = {type : "confirmation", discount: "10", email: "juanjo2895@hotmail.com"}
-    const mail = await mailer(info);
-    return res.json(mail)
+route.get("/email", verify_admin_token, async (req, res) => {
+  jwt.verify(req.token, process.env.SECRET_KEY, async(error, authData) => {
+    if(error){
+      console.log(error);
+      res.status(403).send({message:"Forbidden Access"});
+    } else {
+      let info = {type : "confirmation", discount: "10", email: "juanjo2895@hotmail.com"}
+      const mail = await mailer(info);
+      return res.json(mail)
+    }
+  })
 });
-route.patch("/newOffer", async (req, res) => {
-    const info = req.body;
-    const genOffer = await newOffer(info);
-    return res.status(200).json("Las ofertas se han actualizado");
-}
-);
+route.patch("/newOffer", verify_admin_token, async (req, res) => {
+  jwt.verify(req.token, process.env.SECRET_KEY, async(error, authData) => {
+    if(error){
+      console.log(error);
+      res.status(403).send({message:"Forbidden Access"});
+    } else {
+      const info = req.body;
+      const genOffer = await newOffer(info);
+      return res.status(200).json("Las ofertas se han actualizado");
+    }
+  })
+});
 module.exports = route;
