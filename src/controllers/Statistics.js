5b94109309c6df2d3bb9ec4851c3ab4d9c1f78c4
llamagustin <86086266+llamagustin@users.noreply.github.com>
2022-04-20 12:11:17 -0300

    statistics

--- src/controllers/Statistics.js
+++ src/controllers/Statistics.js
@@ -9,8 +9,37 @@ const statistics = {
                     orderStatus: 'Completed'
                 }
             });
-            const totalOrdersCount = totalOrders.length;
+            const totalOrdersInProcess = await PurchaseOrder.count({
+                where: {
+                    orderStatus: 'Processing'
+                }
+            });
+            const totalOrdersSubmited = await PurchaseOrder.count({
+                where: {
+                    orderStatus: 'Submited'
+                }
+            });
+            const totalOrdersCount = totalOrders.reduce((acc, cur) => acc + cur.orderDetails.length, 0);
             const totalOrdersSum = totalOrders.reduce((acc, cur) => acc + cur.total, 0);
+
+            const totalClients = await Client.count();
+            const totalClientsRegistered = await Client.count({
+                where: {
+                    isRegistered: true
+                }
+            });
+            const totalClientsVerified = await Client.count({
+                where: {
+                    isRegistered: true,
+                    isVerified: true
+                }
+            });
+            const totalClientsAnonymous = await Client.count({
+                where: {
+                    isRegistered: false,
+                    isVerified: false
+                }
+            });
 
             return orders
         }
