c3ec91c8fe11e48beff70f470dc2b539cee75783
revivocelulares <71577083+revivocelulares@users.noreply.github.com>
2022-04-20 03:25:15 -0300

    Update statistics.js

--- src/routes/statistics.js
+++ src/routes/statistics.js
@@ -1,37 +1,58 @@
 const route = require("express").Router();
-const { 
+const {
     getSearchTerms,
     addSearchTerm,
     getOrders
  } = require('../controllers/Statistics');
+ const verify_admin_token = require('../controllers/verify_admin_token.js');
+ const jwt = require('jsonwebtoken');
+ require('dotenv').config();
 
- route.get("/getOrders", async (req, res) => {
-    try {
-        const response = await getOrders();
-        return res.status(200).json(response);
-    } catch (error) {
-        return res.status(500).json("rompiste todo")
+ route.get("/getOrders", verify_admin_token, async (req, res) => {
+   jwt.verify(req.token, process.env.SECRET_KEY, async (error, authData) => {
+     if(error){
+       res.status(403).send({message:"Forbidden Access"});
+     } else {
+      try {
+          const response = await getOrders();
+          return res.status(200).json({response, message:"Authorized Access", authData});
+      } catch (error) {
+          return res.status(500).json("rompiste todo")
+      }
     }
+  })
 });
 
- route.get("/getSearchTerms", async (req, res) => {
-    try {
-        const response = await getSearchTerms();
-        return res.status(200).json(response);
-    } catch (error) {
-        return res.status(500).json("rompiste todo")
+ route.get("/getSearchTerms", verify_admin_token, async (req, res) => {
+   jwt.verify(req.token, process.env.SECRET_KEY, async (error, authData) => {
+     if(error){
+       res.status(403).send({message:"Forbidden Access"});
+     } else {
+      try {
+          const response = await getSearchTerms();
+          return res.status(200).json({response, message:"Authorized Access", authData});
+      } catch (error) {
+          return res.status(500).json("rompiste todo")
+      }
     }
+  })
 });
 
- route.post("/addSearchTerm", async (req, res) => {
+ route.post("/addSearchTerm", verify_admin_token, async (req, res) => {
+   jwt.verify(req.token, process.env.SECRET_KEY, async (error, authData) => {
+     if(error){
+       res.status(403).send({message:"Forbidden Access"});
+     } else {
      const { term } = req.query;
      console.log(term);
-    try {
-        const response = await addSearchTerm(term);
-        return res.status(200).json(response);
-    } catch (error) {
-        return res.status(500).json("rompiste todo")
+      try {
+          const response = await addSearchTerm(term);
+          return res.status(200).json({response, message:"Authorized Access", authData});
+      } catch (error) {
+          return res.status(500).json("rompiste todo")
+      }
     }
+  })
 });
 
 module.exports = route;
