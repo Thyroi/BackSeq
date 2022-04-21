67bbfafe51490c008b1ad72f971db00e374f7338
revivocelulares <71577083+revivocelulares@users.noreply.github.com>
2022-04-21 04:46:46 -0300

    Update login.js

--- src/routes/login.js
+++ src/routes/login.js
@@ -5,36 +5,45 @@ require('dotenv').config();
 
 
 route.post('/', async (req, res) => {
-  const {login_name, login_password} = req.body;
-  const client = await Client.findOne({
-    where: {
-      login_name: login_name,
-      login_password: login_password
+  try{
+    const {login_name, login_password} = req.body;
+    const client = await Client.findOne({
+      where: {
+        login_name: login_name,
+        login_password: login_password
+      }
+    });
+    if(!client){
+      res.json({message: "Incorrect login name or password"});
+    } else {
+      jwt.sign({client}, process.env.SECRET_KEY, (err, token) => {
+        res.json({token, client});
+      })
     }
-  });
-  if(client){
-    jwt.sign({client}, process.env.SECRET_KEY, (err, token) => {
-      res.json({token, client});
-    })
-  } else {
-    res.json({message: "Incorrect login name or password"});
-}
+  } catch (error){
+    res.json({message: "Incorrect User or Password"})
+  }
 });
 
 route.post('/admin', async (req, res) => {
-  const {user_name} = req.body;
-  const user = await Users.findOne({
-    where: {
-      user_name: user_name
+  try{
+      const {user_name, user_password} = req.body;
+      const user = await Users.findOne({
+        where: {
+          user_name: user_name,
+          user_password: user_password
+        }
+      });
+      if(!user) {
+        res.json({message: "Incorrect user or password"});
+      } else {
+        jwt.sign({user}, process.env.SECRET_KEY, (err, token) => {
+          res.json({token, user});
+        })
+      }
+    } catch (error) {
+      res.json({message: "Incorrect user or password"});
     }
-  });
-  if(user) {
-    jwt.sign({user}, process.env.SECRET_KEY, (err, token) => {
-      res.json({token, user});
-    })
-  } else {
-    res.json({message: "Incorrect user or password"});
-  }
 });
 
 module.exports = route;
